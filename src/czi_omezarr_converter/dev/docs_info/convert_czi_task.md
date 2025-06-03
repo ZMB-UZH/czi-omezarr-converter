a4d6eb7d1be643e0f9e30e00d7fb097e32afe8f7
Flurin Sturzenegger <st.flurin@gmail.com>
2025-06-03 17:05:56 +0200

    cleanup

---
+++ src/czi_omezarr_converter/dev/docs_info/convert_czi_task.md
@@ -0,0 +1,10 @@
+### Purpose
+- Convert a Zeiss .czi file OME-Zarr file.
+
+### Outputs
+- An OME-Zarr image.
+
+### Limitations
+- This is only a very rough first implementation of the task.
+
+### Expected inputs
--- src/czi_omezarr_converter/dev/task_list.py
+++ src/czi_omezarr_converter/dev/task_list.py
@@ -1,20 +1,26 @@
 """Contains the list of tasks available to fractal."""
 
 from fractal_task_tools.task_models import (
-    ParallelTask,
+    ConverterCompoundTask,
 )
 
-AUTHORS = "Fractal Core Team"
+AUTHORS = "Flurin Sturzenegger"
 DOCS_LINK = None
 INPUT_MODELS = []
 
 TASK_LIST = [
-    ParallelTask(
-        name="Thresholding Label Task",
-        executable="thresholding_label_task.py",
-        meta={"cpus_per_task": 1, "mem": 4000},
-        category="Segmentation",
-        tags=["Instance Segmentation", "Classical segmentation"],
-        docs_info="file:docs_info/thresholding_task.md",
+    ConverterCompoundTask(
+        name="Convert czi to OME-Zarr",
+        executable_init="convert_czi_init_task.py",
+        executable="convert_czi_compute_task.py",
+        meta_init={"cpus_per_task": 1, "mem": 4000},
+        meta={"cpus_per_task": 1, "mem": 12000},
+        category="Conversion",
+        tags=[
+            "Zeiss",
+            "CZI",
+            "Converter",
+        ],
+        docs_info="file:docs_info/convert_czi_task.md",
     ),
 ]
