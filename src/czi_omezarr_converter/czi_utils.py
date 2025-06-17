1ab1314e6db199b94445924a257e7c1317b4fc2f
Flurin Sturzenegger <st.flurin@gmail.com>
2025-06-17 17:06:36 +0200

    read channel colors from metadata

--- src/czi_omezarr_converter/czi_utils.py
+++ src/czi_omezarr_converter/czi_utils.py
@@ -145,6 +145,28 @@ def build_tiles(czi_path: str | Path, scene_id: int) -> Generator[Tile, Any, Non
         yield tile
 
 
+def get_channel_color(metadata_element, channel_name):
+    path = [
+        "Metadata",
+        "Information",
+        "Image",
+        "Dimensions",
+        "Channels",
+        f'Channel[@Name="{channel_name}"]',
+        "Color",
+    ]
+    el = metadata_element
+    for tag in path:
+        try:
+            el = el.find(tag)
+        except AttributeError:
+            el = None
+            break
+    if el is not None:
+        return el.text[3:]
+    return None
+
+
 def build_tiled_image(
     czi_path: str | Path,
     zarr_name: str | None = None,
@@ -163,6 +185,9 @@ def build_tiled_image(
     channel_names = img.channel_names
     # TODO: check with other test data if this is reasonable
     wavelength_ids = [name.split("-")[0] for name in channel_names]
+    channel_colors = [get_channel_color(img.metadata, name) for name in channel_names]
+    if None in channel_colors:
+        channel_colors = None
 
     if plate:
         # TODO:
@@ -182,6 +207,7 @@ def build_tiled_image(
         path_builder=_path_builder,
         channel_names=channel_names,
         wavelength_ids=wavelength_ids,
+        channel_colors=channel_colors,
     )
     for tile in build_tiles(czi_path, scene_id):
         tiled_image.add_tile(tile)
